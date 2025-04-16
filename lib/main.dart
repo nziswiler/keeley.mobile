94333152f0e6e80d985be4c51c827e3b7e1213ef
nziswiler <nino1999@bluewin.ch>
2025-04-16 20:16:35 +0200

    Architecture: Implement Presentation Layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
+import 'config/routes/app_router.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    // Wir umschließen die gesamte App mit einem ProviderScope,
+    // damit Riverpod für alle Widgets verfügbar ist
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return ShadApp.material(
-      debugShowCheckedModeBanner: false,
-      theme: ShadThemeData(
-        brightness: Brightness.light,
-        colorScheme: const ShadZincColorScheme.light(),
-      ),
-      darkTheme: ShadThemeData(
-        brightness: Brightness.dark,
-        colorScheme: const ShadZincColorScheme.dark(),
-        // Example of custom font family
-        // textTheme: ShadTextTheme(family: 'UbuntuMono'),
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-const frameworks = {
-  'next': 'Next.js',
-  'react': 'React',
-  'astro': 'Astro',
-  'nuxt': 'Nuxt.js',
-};
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ShadCard(
-      width: 350,
-      title: Text(
-        'Create project',
-      ),
-      description: const Text('Deploy your new project in one-click.'),
-      footer: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          ShadButton.outline(
-            child: const Text('Cancel'),
-            onPressed: () {},
-          ),
-          ShadButton(
-            child: const Text('Deploy'),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ShadInputFormField(
-              id: 'username',
-              label: const Text('Username'),
-              placeholder: const Text('Enter your username'),
-              description: const Text('This is your public display name.'),
-              validator: (v) {
-                if (v.length < 2) {
-                  return 'Username must be at least 2 characters.';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 6),
-            const Text('Framework'),
-            ShadSelect<String>(
-              placeholder: const Text('Select'),
-              options: frameworks.entries
-                  .map((e) => ShadOption(value: e.key, child: Text(e.value)))
-                  .toList(),
-              selectedOptionBuilder: (context, value) {
-                return Text(frameworks[value]!);
-              },
-              onChanged: (value) {},
-            ),
-          ],
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ShadApp(
+      builder: (lightTheme, darkTheme) => MaterialApp.router(
+        title: 'Keeley',
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
+        themeMode: ThemeMode.system,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       ),
     );
   }
